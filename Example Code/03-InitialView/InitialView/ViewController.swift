618662871e9e6ffa45db6ca27d7ed9cc8bc81fd6
Mike Ziray <Mike@ElectronicArmory.com>
2019-09-05 21:56:09 -0600

    Textfield and UIButton example code.

--- Example Code/03-InitialView/InitialView/ViewController.swift
+++ Example Code/03-InitialView/InitialView/ViewController.swift
@@ -13,13 +13,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var helloLabel: UILabel!
     
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        helloLabel.text = "Hello Boise State"
+//        helloLabel.text = "Boise State "
     }
 
 
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let username = usernameTextfield.text
+        let password = passwordTextfield.text
+        
+        outputLabel.text = "Logging in: \(username!) with password: \(password)"
+        
+    }
 }
 
 
