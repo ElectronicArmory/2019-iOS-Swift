b28dd6485e69793f068923f81a493774c96efb96
Mike Ziray <Mike@ElectronicArmory.com>
2019-09-20 08:51:58 -0600

    Adding example code for CocoaPods

---
+++ Example Code/04-CococaPods/CococaPods/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CococaPods
+//
+//  Created by Mike Z on 9/19/19.
+//  Copyright © 2019 Boise State. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
